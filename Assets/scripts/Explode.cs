a4c5d91d2144ba6ab5f6362d43f73944013f7c90
OMVR Alienware Laptop <phas@oticonmedical.com>
2025-03-04 09:28:26 +0100

    Cleaning up the Explode class

--- Assets/scripts/Explode.cs
+++ Assets/scripts/Explode.cs
@@ -13,7 +13,7 @@ public class Explode : Projectile
     // explosion management
     public GameObject explosionEffect;
     public float explosionRadius = 5f;
-    public float explosionForce = 15f;
+    public float explosionForce = 0.5f;
     private bool explosionActivated = false;
     
     private bool isLaunched = false;
@@ -21,6 +21,9 @@ public class Explode : Projectile
     public Text countdownText;
     public LifeManager livesManager;
     public PowerUpManager PowerUpManager;
+    public GameObject level1Set;
+    public GameObject level2Set;
+    private bool lastLevel;
 
     void Update()
     {
@@ -29,12 +32,12 @@ public class Explode : Projectile
         }
 
         // Trigger powerup if you have any
-        if (isLaunched && !explosionActivated && Input.GetKeyDown(KeyCode.Space))
+        if (!explosionActivated && Input.GetKeyDown(KeyCode.Space))
         {
-            if (PowerUpManager.Instance.HasPowerUp(PowerUpType.Explosion))
+            if (PowerUpManager.Instance.HasPowerUp(PowerUpType.Blast))
             {
-                StartCoroutine(StartExplosionCountdown());
-                PowerUpManager.Instance.UsePowerUp(PowerUpType.Explosion);
+                StartCoroutine(StartBlastCountdown());
+                PowerUpManager.Instance.UsePowerUp(PowerUpType.Blast);
             }
         }
     }
@@ -42,20 +45,23 @@ public class Explode : Projectile
     public override void Start()
     {
         base.Start();
+        lastLevel = false;
 
         // Store the boxes and their original positions to reset post destruction
-        GameObject[] allBoxes = GameObject.FindGameObjectsWithTag("Box");
-        foreach (var box in allBoxes){
-            boxes.Add(box);
-            boxPositions.Add(box.transform.position);
-        }
+        SetupBoxes();
     }
 
+    /// <summary>
+    /// Manage behavior when projectile collides with something.
+    /// Anything with the "tag" box is set to explode.
+    /// If the throw misses and the projectile hits the ground, the projectile resets. 
+    /// </summary>
     public override void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Box"))
         {
-            ExplodeNearbyBoxes();
+            ExplodeNearbyBoxes(0.5f, explosionForce);
+            livesManager.UseLife();
             return;
         }
 
@@ -64,54 +70,10 @@ public class Explode : Projectile
             if (timeAfterLaunch > 0.5f)
             {
                 StartCoroutine(DisappearWithPuff());
+                livesManager.UseLife();
+                return;
             }
         }
-    }
-
-    IEnumerator StartExplosionCountdown()
-    {
-        explosionActivated = true;
-        
-        for (int i = 3; i > 0; i--)
-        {
-            if (countdownText != null){
-                countdownText.text = "Explosion in: " + i;
-            }
-            yield return new WaitForSeconds(1f);
-        }
-
-        if (countdownText != null){
-            countdownText.text = "";  // Clear text 
-        }
-        applyExplode();
-    }
-
-    /// <summary>
-    /// Generate an explosion on collision with boxes.
-    /// </summary>
-    void applyExplode()
-    {
-        if (explosionEffect != null)
-        {
-            GameObject explosion = Instantiate(explosionEffect, transform.position, Quaternion.identity);
-            Destroy(explosion, 2f);
-        }
-
-        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
-        foreach (Collider nearby in colliders)
-        {
-            Rigidbody rb = nearby.GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
-            }
-
-            if (nearby.CompareTag("Box")){
-                Destroy(nearby.gameObject);
-            }
-        }
-        Destroy(gameObject);
-        StartCoroutine(ResetAfterExplosion());
     }
 
     public override void OnMouseUp()
@@ -120,11 +82,42 @@ public class Explode : Projectile
         {
             isLaunched = true;
             base.OnMouseUp();
-            livesManager.UseLife();
         }
     }
 
-    void ExplodeNearbyBoxes()
+    /// <summary>
+    /// Create a timer to announce the big blast will occur in 3 seconds.
+    /// </summary>
+    IEnumerator StartBlastCountdown()
+    {
+        explosionActivated = true;
+        
+        for (int i = 3; i > 0; i--)
+        {
+            if (countdownText != null){
+                countdownText.text = "Blast in: " + i;
+            }
+            yield return new WaitForSeconds(1f);
+        }
+
+        if (countdownText != null){
+            countdownText.text = "";  // Clear text 
+        }
+
+        // blast with a bigger radius and force
+        if (explosionEffect != null)
+        {
+            GameObject explosion = Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            Destroy(explosion, 2f);
+        }
+        ExplodeNearbyBoxes(5f, explosionForce * 1.5f);
+    }
+
+    /// <summary>
+    /// If projectile collides with boxes, create an explosion
+    /// and check if it propagates to nearby boxes
+    /// </summary>
+    void ExplodeNearbyBoxes(float radius, float force)
     {
         List<GameObject> destroyedBoxes = new List<GameObject>();
 
@@ -141,23 +134,37 @@ public class Explode : Projectile
                 Destroy(box);
             }
         }
+
+        // clear up box gameobjects
+        foreach (GameObject box in destroyedBoxes){
+            boxes.Remove(box);
+        }
+
+        // adjust how many boxes in the vicinity are affected by explosion
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider nearby in colliders)
+        {
+            Rigidbody rb = nearby.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+
+            if (nearby.CompareTag("Box")){
+                Destroy(nearby.gameObject);
+            }
+        }
+
+        // clear up box gameobjects
         foreach (GameObject box in destroyedBoxes){
             boxes.Remove(box);
         }
 
         CheckWinCondition();
-        if (livesManager.currentLives >= 0){
+        if (livesManager.currentLives > 0){
             ResetProjectile();
         }
-        else{
-            livesManager.GameOver();
-
-            // check if user wants to go again
-            bool restart = RestartGame();
-            if(restart == true){
-                Start();
-            }
-        }
+        StartCoroutine(ResetAfterExplosion());
     }
 
     /// <summary>
@@ -168,9 +175,47 @@ public class Explode : Projectile
         if (boxes.Count == 0)
         {
             Debug.Log("Congrats, you've won BombsAway! Power-Up Earned :)");
-            PowerUpManager.Instance.EarnPowerUp(PowerUpType.Explosion);
-            RespawnBoxes();
+            PowerUpManager.Instance.EarnPowerUp(PowerUpType.Blast);
             livesManager.WinGame();
+            
+            StartCoroutine(TransitionToNextLevel());
+            lastLevel = true;
+            return;
+        }
+        else
+        {
+            if (livesManager.currentLives == 0 && lastLevel== true)
+            {
+                livesManager.GameOver();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Add a timer in between levels switch so boxes don't get counted accidentally and trigger a game over.
+    /// </summary>
+    IEnumerator TransitionToNextLevel()
+    {
+        yield return new WaitForSeconds(2f);
+        ResetProjectile();
+        boxes.Clear(); 
+
+        // Move to next level
+        level1Set.SetActive(false);
+        level2Set.SetActive(true);
+        SetupBoxes();
+    }
+
+    // RESETING -----------------------------------
+    /// <summary>
+    /// Find all gameobjects with the box tag and storing them.
+    /// </summary>
+    void SetupBoxes()
+    {
+     GameObject[] allBoxes = GameObject.FindGameObjectsWithTag("Box");
+        foreach (var box in allBoxes){
+            boxes.Add(box);
+            boxPositions.Add(box.transform.position);
         }
     }
 
